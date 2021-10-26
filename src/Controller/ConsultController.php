29be342989baed1eb4645a5922d25a18272a75f1
Mukaarts <fmichael.1995@gmail.com>
2021-10-26 15:31:32 +0200

    add Controllers

---
+++ src/Controller/ConsultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConsultController extends AbstractController
+{
+    #[Route('/consult', name: 'consult')]
+    public function index(): Response
+    {
+        return $this->render('consult/index.html.twig', [
+            'controller_name' => 'ConsultController',
+        ]);
+    }
+}
